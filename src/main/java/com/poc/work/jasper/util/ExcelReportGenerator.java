ac486c8e5354561416c2cd81cdf4ca5e3eb6be93
Heydie Zuliansyah <helsarayta@gmail.com>
2024-04-16 12:15:24 +0700

    testd

--- src/main/java/com/poc/work/jasper/util/ExcelReportGenerator.java
+++ src/main/java/com/poc/work/jasper/util/ExcelReportGenerator.java
@@ -55,7 +55,8 @@ public class ExcelReportGenerator {
 							.getBytes(StandardCharsets.UTF_8)));
 
 //			comment test data
-			
+//			comment test data
+
 			JasperReport jasperReport = JasperCompileManager.compileReport("src/main/resources/report/cineverse_statement.jrxml");
 			Map<String, Object> requestReportConfig = new HashMap<>();
 			
